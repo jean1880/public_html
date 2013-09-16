c900b91727c27581988ac24fcb001694b9c07a8e
Jean-Luc Desroches <jean18801880@gmail.com>
2013-09-16 12:41:56 -0400

    commit part 6

---
+++ IntrotoHTMLClass/trolls/scripts/wojodesign-simplecart-js-bc8eacb/_vti_cnf/README.md
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|10 Dec 2012 00:42:00 -0000
+vti_author:SR|ALPHAPC-PC\\Alphapc
+vti_modifiedby:SR|ALPHAPC-PC\\Alphapc
+vti_nexttolasttimemodified:TW|10 Dec 2012 00:42:00 -0000
+vti_timecreated:TR|17 Jan 2013 06:33:38 -0000
+vti_cacheddtm:TX|17 Jan 2013 06:33:38 -0000
+vti_filesize:IR|7869
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_syncwith_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TX|17 Jan 2013 06:33:38 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TW|15 Feb 2013 15:12:00 -0000
+vti_syncwith_webdesign4.georgianc.on.ca\:21:TX|17 Jan 2013 06:33:38 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21:TW|18 Jan 2013 19:07:00 -0000
---
+++ IntrotoHTMLClass/trolls/scripts/wojodesign-simplecart-js-bc8eacb/test/inc/_vti_cnf/get-raw-javascript.php
@@ -0,0 +1,19 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|10 Dec 2012 00:42:00 -0000
+vti_author:SR|ALPHAPC-PC\\Alphapc
+vti_modifiedby:SR|ALPHAPC-PC\\Alphapc
+vti_nexttolasttimemodified:TW|10 Dec 2012 00:42:00 -0000
+vti_timecreated:TR|17 Jan 2013 06:33:46 -0000
+vti_cacheddtm:TX|17 Jan 2013 06:33:46 -0000
+vti_filesize:IR|138
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_syncwith_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TX|17 Jan 2013 06:33:46 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TW|15 Feb 2013 15:12:00 -0000
+vti_syncwith_webdesign4.georgianc.on.ca\:21:TX|17 Jan 2013 06:33:46 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21:TW|18 Jan 2013 19:07:00 -0000
