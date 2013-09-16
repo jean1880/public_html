5d79735a959569399c11ba9dc0b146c519a9c8d7
Jean-Luc Desroches <jean18801880@gmail.com>
2013-09-16 12:49:08 -0400

    commit part 9

---
+++ WebProgrammingClass/_vti_cnf/PHP_Database.php
@@ -0,0 +1,21 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ALPHAPC-PC\\Alphapc
+vti_modifiedby:SR|ALPHAPC-PC\\Alphapc
+vti_timelastmodified:TR|12 Feb 2013 23:16:06 -0000
+vti_timecreated:TR|29 Jan 2013 23:40:55 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|07 Feb 2013 22:07:04 -0000
+vti_syncwith_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TX|07 Feb 2013 22:07:04 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TW|15 Feb 2013 15:12:00 -0000
+vti_cacheddtm:TX|07 Feb 2013 22:07:04 -0000
+vti_filesize:IR|2926
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|A|<
+vti_cachedsvcrellinks:VX|DAUS|Web\\ Programming\\ Lessons/<
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=UTF-8
+vti_charset:SR|utf-8
---
+++ WebProgrammingClass/_vti_cnf/index.php
@@ -0,0 +1,23 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ALPHAPC-PC\\Alphapc
+vti_modifiedby:SR|ALPHAPC-PC\\Alphapc
+vti_timelastmodified:TR|12 Feb 2013 23:16:06 -0000
+vti_timecreated:TR|15 Jan 2013 23:49:18 -0000
+vti_title:SR|Untitled 1
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|17 Jan 2013 02:08:01 -0000
+vti_syncwith_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TX|17 Jan 2013 02:08:01 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21/%2fpublic_html:TW|15 Feb 2013 15:12:00 -0000
+vti_syncwith_webdesign4.georgianc.on.ca\:21:TX|17 Jan 2013 02:08:01 -0000
+vti_syncofs_webdesign4.georgianc.on.ca\:21:TW|18 Jan 2013 19:07:00 -0000
+vti_cacheddtm:TX|17 Jan 2013 02:08:01 -0000
+vti_filesize:IR|533
+vti_cachedtitle:SR|Untitled 1
+vti_cachedbodystyle:SR|<body>
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
