e7564a05497e4dd6a879e9af784128895f1409d5
jean1880 <jean18801880@gmail.com>
2013-11-02 22:37:10 -0400

    commenting
    updated a few comments

--- DATA/JavaScripts/mobile.nivo.js
+++ DATA/JavaScripts/mobile.nivo.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 $(document).load(function(){
+	// detect if user is swiping left or right and display appropriate image
 	$("#wrapper").bind("swipeleft","ui-link",function() {
 		$('.nivo-nextNav').trigger('click');
 		alert('test');
